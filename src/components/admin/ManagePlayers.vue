31abe19c1b6f585662978531c8a2e195b10b16d2
dazbo <dazbo@users.noreply.github.com>
2018-04-10 16:06:08 +0200

    Update icons Add import/export

--- src/components/admin/ManagePlayers.vue
+++ src/components/admin/ManagePlayers.vue
@@ -1,28 +1,40 @@
 <template>
-    <div>
-        <ul class="list-group">
-            <li class="list-group-item" v-for="player in players" :key="player.battletag">
-                <div class="row">
-                    <div class="col-6 col-sm">
-                        <strong>{{player.battletag}}</strong>
-                    </div>
-                    <div class="col-6 col-sm">
-                        <timeago :since="player.modifiedOn"></timeago>
-                    </div>
-                    <div class="col-12 col-sm text-right">
-                        <div class="btn-group" role="group">
-                            <button :disabled="isBusy" type="button" class="btn btn-primary" @click="refreshProfil({battletag:player.battletag, region:player.region, platform:player.platform })">Mettre à jour</button>
-                            <button :disabled="isBusy" type="button" class="btn btn-danger" @click="removeProfil({battletag:player.battletag, region:player.region, platform:player.platform })">Supprimer</button>
-                        </div>
-                    </div>
-                </div>
-            </li>
-            <AddPlayerRow @playerAdded="getProfiles" />
-        </ul>
-        <div class="text-right">
-            <button :disabled="isBusy" type="button" class="btn btn-light" @click="updateAll">Tout mettre à jour</button>
+  <div>
+    <ul class="list-group">
+      <li class="list-group-item" v-for="player in players" :key="player.battletag">
+        <div class="row">
+          <div class="col-6 col-sm">
+            <strong>{{player.battletag}}</strong>
+          </div>
+          <div class="col-6 col-sm">
+            <timeago :since="player.modifiedOn"></timeago>
+          </div>
+          <div class="col-12 col-sm text-right">
+            <div class="btn-group" role="group">
+              <button :disabled="isBusy" type="button" class="btn btn-primary" @click="refreshProfil({battletag:player.battletag, region:player.region, platform:player.platform })">Mettre à jour</button>
+              <button :disabled="isBusy" type="button" class="btn btn-danger" @click="removeProfil({battletag:player.battletag, region:player.region, platform:player.platform })">Supprimer</button>
+            </div>
+          </div>
         </div>
+      </li>
+      <AddPlayerRow @playerAdded="getProfiles" />
+    </ul>
+    <div class="text-right">
+      <button :disabled="isBusy" type="button" class="btn btn-light" @click="updateAll">Tout mettre à jour</button>
+      <button :disabled="isBusy" type="button" class="btn btn-light" data-toggle="collapse" data-target="#collapseExample">Import/Export</button>
     </div>
+    <div class="collapse" id="collapseExample">
+      <div class="card">
+        <div class="card-body">
+          <textarea class="form-control" name="import" id="import" rows="8" v-model="currentPlayers" />
+        </div>
+        <div class="card-body">
+          <a href="#" class="card-link" @click="importPlayers">Import</a>
+        </div>
+      </div>
+
+    </div>
+  </div>
 
 </template>
 
@@ -36,7 +48,8 @@ export default {
   data() {
     return {
       players: [],
-      isBusy: false
+      isBusy: false,
+      currentPlayers: []
     };
   },
   mounted() {
@@ -52,6 +65,9 @@ export default {
           modifiedOn: new Date(p.modifiedOn)
         };
       });
+      this.currentPlayers = this.players
+        .map(p => [p.battletag, p.region, p.platform].join(";"))
+        .join("\n");
     },
     refreshProfil(player) {
       this.isBusy = true;
@@ -73,6 +89,20 @@ export default {
       this.players.forEach(p => tasks.push(playerServices.updatePlayer(p)));
       Promise.all(tasks).then(result => this.getProfiles());
       this.isBusy = false;
+    },
+    importPlayers() {
+      this.currentPlayers.split("\n").forEach(p => {
+        const [battletag, region, platform] = p.split(";");
+        const player = { battletag, platform, region };
+        playerServices
+          .addProfile(player)
+          .then(data => {
+            this.$emit("playerAdded", data);
+          })
+          .catch(err => {
+            this.error = err;
+          });
+      });
     }
   }
 };
